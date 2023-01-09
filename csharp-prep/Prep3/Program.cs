ed5e33ce0295024cdaa21e4817d66a663c7dd579
EzequielRindello <rindelloezequiel@gmail.com>
2023-01-09 16:28:45 -0300

    guess game finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int ramdom_number = randomGenerator.Next(1, 11);
+        int guess_count = 0;
+        Console.WriteLine("Welcome to the guees game!");
+        Console.WriteLine("Try to guees the magic number between 1 and 10 :) ");
+
+        while (true)
+        {
+            Console.WriteLine("What is the magic number? ");
+            string guess = Console.ReadLine();
+            int valid = int.Parse(guess);
+            if (valid > ramdom_number)
+            {
+                Console.WriteLine("Too High");
+                guess_count = guess_count + 1;
+            }
+            else if (valid < ramdom_number)
+            {
+                Console.WriteLine("Too Low");
+                guess_count = guess_count + 1;
+            }
+            else
+            {
+                guess_count = guess_count + 1;
+                break;
+            }
+        }
+        Console.WriteLine($"You guessed it! It took you {guess_count} guesses");
     }
 }
