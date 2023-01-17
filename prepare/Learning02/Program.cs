c38066e7c749f9dd0a5ed3aa341ec3094f62ba1d
EzequielRindello <rindelloezequiel@gmail.com>
2023-01-17 12:06:39 -0300

    Final work.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -10,10 +10,10 @@ class Program
         job1._endYear = 2019;
 
         Job job2 = new Job();
-        job1._jobTitle = "Operario";
-        job1._company = "Jhon Deere";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job2._jobTitle = "Operario";
+        job2._company = "Jhon Deere";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
 
         Resume myResume = new Resume();
         myResume._name = "Ezequiel Rindello";
