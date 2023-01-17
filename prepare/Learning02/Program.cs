0d4e0c3c92cb276f01c8008ddcbfd70f7e1c8df7
EzequielRindello <rindelloezequiel@gmail.com>
2023-01-17 11:59:10 -0300

    Test changes.

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -3,6 +3,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        Job job1 = new Job();
+        job1._jobTitle = "Tornero";
+        job1._company = "Rinaudo";
+        job1._startYear = 2019;
+        job1._endYear = 2019;
 
+        Job job2 = new Job();
+        job1._jobTitle = "Operario";
+        job1._company = "Jhon Deere";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Ezequiel Rindello";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
