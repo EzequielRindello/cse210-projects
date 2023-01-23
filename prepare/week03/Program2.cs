1d0d60483ab311152193716965ae2dfc94270bd0
EzequielRindello <rindelloezequiel@gmail.com>
2023-01-23 13:09:46 -0300

    promp selector ready

--- prepare/week03/Program2.cs
+++ prepare/week03/Program2.cs
@@ -42,17 +42,8 @@ class Program2
     }
     static void CaseOne()
     {
-        var list = new List<string>{ "What brings you joy? ", "Describe a place where you felt happiest. ",
-        "What is something that you would like to change about yourself? How can you change it? ",
-        "If you are granted a wish, what would you wish for and why? ",
-        "How could you make someone you care about feel better if he/she just lost something important to them? " };
-
-        PromptGenerator prompt1 = new PromptGenerator();
-        Random r = new Random();
-        int index = r.Next(list.Count);
-        string randomString = list[index];
-        prompt1._prompt = randomString;
-        prompt1.DisplayPrompt();
+        PromptGenerator prompt = new PromptGenerator();
+        prompt.selectPromp();
     }
 
 }
