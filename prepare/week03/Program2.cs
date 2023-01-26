e6a28d6bcaacd94bafbead2b5baacf929b624334
EzequielRindello <rindelloezequiel@gmail.com>
2023-01-26 12:47:20 -0300

    ...

--- prepare/week03/Program2.cs
+++ prepare/week03/Program2.cs
@@ -7,8 +7,9 @@ class Program2
 
         // Global variables.
         bool valid = false;
-        string prompt="";
-        string userAnswer="";
+        string prompt = "";
+        string userResponse = "";
+        string time = "";
         // Creates a while loop whit a boolean to keep displaying options for the user.
         // Once the user is done with the program the loop finishes.
         while (valid != true)
@@ -19,10 +20,11 @@ class Program2
             {
                 case "1":
                     prompt = PromptGenerator();
-                    userAnswer = GetResponse();
+                    userResponse = GetResponse();
+                    time = GetTime();
                     break;
                 case "2":
-                    Display(prompt, userAnswer);
+                    Display(prompt, userResponse);
                     break;
                 case "3":
                     break;
@@ -64,10 +66,15 @@ class Program2
         return answer;
     }
 
-    static string Display(string prompt, string userAnswer)
+    static string GetTime()
     {
+        return null;
+    }
+    static string Display(string prompt, string userResponse)
+    {
+        // Display the prompt, the user response and time.
         Journal display1 = new Journal();
-        display1.DisplayResponse(prompt, userAnswer);
+        display1.DisplayResponse(prompt, userResponse);
         return null;
     }
 }
