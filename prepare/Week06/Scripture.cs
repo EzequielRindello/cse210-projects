129dedce6c7f08f1f19f3bde6bb0ff5451191b28
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-03 11:24:42 -0300

    preparando semana 6

--- prepare/Week06/Scripture.cs
+++ prepare/Week06/Scripture.cs
@@ -2,5 +2,7 @@ using System.IO;
 
 public class Scripture
 {
-
+    private  List<string> Proverbs = new List<string>() { "Trust ", "in ", "the ", "Lord ", "with ", "all ", "thine ", "heart;", " and ", "lean "
+    , "not ", "unto ", "thine ", "own ", "understanding.", "In ", "all ", "thy ", "ways ",
+    "acknowledge ", "him, ", "and ", "he ", "shall ", "direct ", "thy ", "paths." };
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,24 +5,42 @@ class Program
     static void Main(string[] args)
     {
         var rand = new Random();
-        string[] movies = { "deadpool", "batmanvssuperman", "findingdory", "titanic", "suicidesquad", "lordoftherings", "harrypotter", "jurassicpark", "hungergames", "despicableme" };
-        List<string> randomMovies = movies.ToList();
+        List<string> Proverbs = new List<string>() { "Trust ", "in ", "the ", "Lord ", "with ", "all ", "thine ", "heart;", " and ", "lean "
+            , "not ", "unto ", "thine ", "own ", "understanding.", "In ", "all ", "thy ", "ways ",
+            "acknowledge ", "him, ", "and ", "he ", "shall ", "direct ", "thy ", "paths." };
 
-        for (int i = 0; i < movies.Length / 2; i++)
+
+        var random = new Random();
+        int index = random.Next(Proverbs.Count);
+        string randomString = Proverbs[index];
+        HashSet<string> discovered = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+        discovered.Add(randomString);
+        string convertWord = ConvertWord(randomString, discovered);
+
+        int fd = Proverbs.FindIndex(s => s == randomString);
+
+        if (fd != -1)
+            Proverbs[fd] = convertWord;
+
+        ;
+
+        DisplayList(Proverbs);
+
+    }
+
+    public List<string> NewList(List<string> Proverbs)
+    {
+        return null;
+    }
+    public static void DisplayList(List<string> Proverbs)
+    {
+        foreach (var x in Proverbs)
         {
-            var randNum = rand.Next(i, randomMovies.Count);
-            var temp = randomMovies[randNum];
-            randomMovies[randNum] = randomMovies[i];
-            randomMovies[i] = temp;
+            Console.Write(x);
         }
-
-        var randomMovie = randomMovies.First();
-        randomMovies.Remove(randomMovie);
-
-        Console.Clear();
-        for (int i = 0; i < randomMovies.Count; i++)
-        {
-            Console.WriteLine(randomMovies[i]);
-        }
+    }
+    static string ConvertWord(string word, HashSet<string> discovered)
+    {
+        return string.Concat(word.Select(p => discovered.Contains(p.ToString()) ? p : '_'));
     }
 }
