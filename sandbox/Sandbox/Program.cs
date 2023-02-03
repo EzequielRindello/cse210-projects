ca817a131e35a5f4726e7224cb2b2737b3c00525
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-03 13:48:16 -0300

    probando

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,31 +6,32 @@ class Program
     {
         var rand = new Random();
         List<string> Proverbs = new List<string>() { "Trust ", "in ", "the ", "Lord ", "with ", "all ", "thine ", "heart;", " and ", "lean "
-            , "not ", "unto ", "thine ", "own ", "understanding.", "In ", "all ", "thy ", "ways ",
-            "acknowledge ", "him, ", "and ", "he ", "shall ", "direct ", "thy ", "paths." };
+        , "not ", "unto ", "thine ", "own ", "understanding.", "In ", "all ", "thy ", "ways ",
+        "acknowledge ", "him, ", "and ", "he ", "shall ", "direct ", "thy ", "paths." };
 
+        NewList(Proverbs);
+        DisplayList(Proverbs);
+        Console.WriteLine();
+        NewList(Proverbs);
+        DisplayList(Proverbs);
 
+    }
+
+    public static List<string> NewList(List<string> Proverbs)
+    {
         var random = new Random();
         int index = random.Next(Proverbs.Count);
         string randomString = Proverbs[index];
         HashSet<string> discovered = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
         discovered.Add(randomString);
         string convertWord = ConvertWord(randomString, discovered);
+        int valid = Proverbs.FindIndex(s => s == randomString);
+        if (valid != -1)
+        {
+            Proverbs[valid] = convertWord;
+        }
 
-        int fd = Proverbs.FindIndex(s => s == randomString);
-
-        if (fd != -1)
-            Proverbs[fd] = convertWord;
-
-        ;
-
-        DisplayList(Proverbs);
-
-    }
-
-    public List<string> NewList(List<string> Proverbs)
-    {
-        return null;
+        return Proverbs;
     }
     public static void DisplayList(List<string> Proverbs)
     {
