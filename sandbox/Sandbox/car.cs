baaacac67e9f012fe76ffb56d0e3457f439cfb7b
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-14 11:07:12 -0300

    .

--- sandbox/Sandbox/car.cs
+++ sandbox/Sandbox/car.cs
@@ -1,5 +1,9 @@
 public class Car
 {
+    private string _moldel;
+    private int _year;
+    private string _name;
+
     public void Display(string model,int year,string name)
     {
         Console.WriteLine($"{model}{name}{year}");
