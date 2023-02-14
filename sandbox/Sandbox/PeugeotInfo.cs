c00c65f42d678350bf6b49d7723b11623649cac8
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-14 12:58:54 -0300

    .

--- sandbox/Sandbox/PeugeotInfo.cs
+++ sandbox/Sandbox/PeugeotInfo.cs
@@ -1,10 +1,12 @@
 public class PeugeotInfo :Car
 {
-    private string _hayquedefinirquehacelaclase;
+    private string _hayquedefinirquehacelaclase;  // pasar un txt con info y la pagina oficial de peogueot
     private string _hayquedefinirquehacelaclase2;
 
     public PeugeotInfo(string name,string model,int year,string hayquedefinirquehacelaclase) : base(name,model,year)
     {
         _hayquedefinirquehacelaclase=hayquedefinirquehacelaclase;
     }
+
+    //el comportamiento de esta clase podria ser display the text especifico de aca
 }
