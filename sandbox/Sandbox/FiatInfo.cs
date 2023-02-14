487bf9a70bbda5cbf1655053fe86463ba4f517bd
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-14 12:58:39 -0300

    .

--- sandbox/Sandbox/FiatInfo.cs
+++ sandbox/Sandbox/FiatInfo.cs
@@ -1,10 +1,13 @@
 public class FiatInfo:Car
 {
-    private string _hayquedefinirquehacelaclase;
+    private string _hayquedefinirquehacelaclase; // pasar un txt con info y la pagina oficial de fiat
     private string _hayquedefinirquehacelaclase2;
 
     public FiatInfo(string name,string model,int year,string hayquedefinirquehacelaclase) : base(name,model,year)
     {
         _hayquedefinirquehacelaclase=hayquedefinirquehacelaclase;
     }
+
+
+    //el comportamiento de esta clase podria ser display the text especifico de aca
 }
