269b9a9d081793d3e6a0b3d8d51c236dffda6929
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-15 12:52:28 -0300

    switch menu.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        while(true){
+            string answer=GetMenuChoice();
+            switch (answer)
+            {
+                case "1":
+                    return;
+                case "2":
+                    return;
+                case "3":
+                    return;
+                default :
+                    Console.WriteLine("Error, please enter a valid choice.");
+                    break;
+            }
+        }
+    }
+
+    public static string GetMenuChoice()
+    {
+        Console.WriteLine("Menu Options:");
+        Console.WriteLine("1:Start breathing activity\n2:Start reflection activity\n3:Start listening activity");
+        return Console.ReadLine();
     }
 }
