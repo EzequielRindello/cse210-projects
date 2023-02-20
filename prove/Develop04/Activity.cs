5b54cc9bb23f4335ca2a0f043aacef86c700e2a1
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-20 14:38:27 -0300

    spinner

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -11,4 +11,52 @@ public class Activity
         _duration = duration;
     }
 
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void GetReadyMessage()
+    {
+
+    }
+
+    public void PauseSpinner()
+    {
+        int counter=0;
+        while (true)
+        {
+            
+            counter++;
+            switch (counter % 4)
+            {
+                case 0: 
+                    Console.Write("/");
+                    Thread.Sleep(650);
+                    break;
+                case 1: 
+                    Console.Write("-"); 
+                    Thread.Sleep(650);
+                    break;
+                case 2: 
+                    Console.Write("\\"); 
+                    Thread.Sleep(650);
+                    break;
+                case 3: 
+                    Console.Write("|"); 
+                    Thread.Sleep(650);
+                    break;
+            }
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+            if (counter==10)
+            {
+                break;
+            }
+        }
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
 }
