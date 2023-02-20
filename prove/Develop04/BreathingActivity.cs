caf365de88481341150e1e578947df5f69f01566
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-20 17:34:13 -0300

    change time

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -6,7 +6,7 @@ public class BreathingActivity : Activity
     {
         _name = "Welcome to the breathing activity.";
         _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
-        _duration = 50;
+        _duration = 30;
     }
 
     public void RunBreathingActivity()
