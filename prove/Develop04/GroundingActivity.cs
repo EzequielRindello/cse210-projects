8ff63b04a22d7b2e403081c049260ff283bbb81f
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-23 13:00:57 -0300

    done!

---
+++ prove/Develop04/GroundingActivity.cs
@@ -0,0 +1,23 @@
+public class GroundingActivity : Activity
+{
+    /* This activity is mentioned in 
+        the group meeting so I decided 
+        to add it to get extra points.
+        it wont have time and atributes to
+        be different from the others :) */
+    public GroundingActivity(string name, string description, int duration) : base(name, description, duration)
+    {
+        _name = "Grounding ctivity";
+        _description = "This five-step exercise can be very helpful during periods of anxiety or panic by helping to ground you in the present when your mind is bouncing around between various anxious thoughts.";
+    }
+
+    public void RunGroundingActivity()
+    {
+
+    }
+
+    public void SpecialEndingMessage()
+    {
+
+    }
+}
