8ccc0d325e495d218182889afa41a20cbe40273b
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-27 13:18:14 -0300

    .

--- sandbox/Sandbox/car.cs
+++ sandbox/Sandbox/car.cs
@@ -4,16 +4,16 @@ public class Car
     protected string _model;
     protected int _year;
 
-    public Car(string a,string b,int c)
+    public Car(string a, string b, int c)
     {
-        _name=a;
-        _model=b;
-        _year=c;
+        _name = a;
+        _model = b;
+        _year = c;
     }
-   
+
     public string GetCarInfo()
     {
         return $"{_name} {_model} {_year}";
     }
-    
-} 
+
+}
