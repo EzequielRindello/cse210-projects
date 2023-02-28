9d758d65782163efa93bba6c500369625f97f07e
EzequielRindello <rindelloezequiel@gmail.com>
2023-02-28 12:59:15 -0300

    menu

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        while (true)
+        {
+            string answer = Menu();
+            switch (answer)
+            {
+                case "1":
+
+                    break;
+                case "2":
+
+                    break;
+                case "3":
+                    break;
+                case "4":
+
+                    break;
+                case "5":
+
+                    break;
+                case "6":
+
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Error, please enter a valid choice.");
+                    Thread.Sleep(4000);
+                    break;
+            }
+        }
     }
+
+    public static string Menu()
+    {
+        Console.WriteLine("1: Create new goal\n2: List Goals\n3: Save goals\n4: Load goals\n25: Record event \n26: Quit ");
+        return Console.ReadLine();
+    }
+
 }
